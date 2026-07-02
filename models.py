a7841b21c74934a5ef96418864543895360e69d3
lionheart <mikeoladapo2004@gmail.com>
2026-07-02 12:56:54 +0100

    models

--- models.py
+++ models.py
@@ -10,7 +10,6 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(engine,expire_on_commit=False)
-session = AsyncSessionLocal()
 
 class Base(DeclarativeBase):
     pass 
