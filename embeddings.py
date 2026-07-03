61f3bcc879d373428fe0d0317d79cc947db15621
lionheart <mikeoladapo2004@gmail.com>
2026-07-03 10:15:21 +0100

    chunking

--- embeddings.py
+++ embeddings.py
@@ -6,7 +6,11 @@ def read_file(path:Path):
     text = ""
     for page in read.pages:
         text += page.extract_text() + "\n"
-        return text 
+    return text 
     
 def chunk(text:str,count:int=20,overlap:int=2):
-    
+    step = count - overlap
+    chunks = []
+    for start in range (0,len(text),step):
+        chunks.append(text[start:start + count])
+    return chunks 
